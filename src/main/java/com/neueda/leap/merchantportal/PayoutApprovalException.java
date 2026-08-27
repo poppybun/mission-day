fab21c540a8c8ee7ac661a002cb4f467467f33a5
Ian Gill <ian123gill@gmail.com>
2026-08-27 09:24:27 +0000

    fix: created file that is now referenced by PayoutApprovalService

---
+++ src/main/java/com/neueda/leap/merchantportal/PayoutApprovalException.java
@@ -0,0 +1,7 @@
+package com.neueda.leap.merchantportal;
+
+public class PayoutApprovalException extends RuntimeException {
+    public PayoutApprovalException(String message) {
+        super(message);
+    }
+}
